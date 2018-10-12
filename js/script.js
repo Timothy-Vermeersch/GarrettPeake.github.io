8e39d9be21a29cc017e9abe77e2acde3441b76ef
Peake <gepeake@gmail.com>
2018-10-11 18:46:49 -0700

    Testing Firebase Saving

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log(window.location.search);
+function saveToFirebase(data) {
+    var dataObject = {
+        DATA: data
+    };
+
+    firebase.database().ref('test').push().set(emailObject)
+        .then(function(snapshot) {
+        }, function(error) {
+            console.log('error' + error);
+        });
+}
+
+saveToFirebase("Test data");
