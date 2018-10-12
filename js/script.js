d02452eb435c81f7b443b044957f8270017aee7c
Peake <gepeake@gmail.com>
2018-10-11 17:59:27 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-document.getElementById("paragraph").textContent="Debug Testing";
+document.getElementById("paragraph").textContent="Debug Test1";
 var url_string = "http://www.example.com/t.html?a=1&b=3&c=m2-m3-m4-m5"; //window.location.href
 var url = new URL(url_string);
 var c = url.searchParams.get("c");
