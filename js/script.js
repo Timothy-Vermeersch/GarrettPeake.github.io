500ee4a17af0deec9c1527e0ec68fd0fac49b5bf
Peake <gepeake@gmail.com>
2018-10-11 17:51:43 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,1 @@
-(function(open) {
-
-    XMLHttpRequest.prototype.open = function(method, url, async, user, pass) {
-
-        this.addEventListener("readystatechange", function() {
-            console.log(this.readyState); // this one I changed
-        }, false);
-
-        open.call(this, method, url, async, user, pass);
-    };
-
-})(XMLHttpRequest.prototype.open);
+document.getElementById("paragraph").textContent="Debug Testing";
