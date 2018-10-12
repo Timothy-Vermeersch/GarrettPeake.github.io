e6f344c0b44ab3b9d0a8eeac0a8ceafbf85b69b2
Peake <gepeake@gmail.com>
2018-10-11 18:07:47 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-document.getElementById("paragraph").textContent="Debug Test1";
-var url_string = "http://www.example.com/t.html?a=1&b=3&c=m2-m3-m4-m5"; //window.location.href
-var url = new URL(url_string);
-var c = url.searchParams.get("c");
-console.log(c);
+
+var client = clientList[0];
+console.log(client.url);
