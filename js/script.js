a508274d290f2bb4c16cbac12538254a6f3c6d5b
Peake <gepeake@gmail.com>
2018-10-11 19:01:08 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,12 +6,12 @@ function error(){
     console.log("failed to write data to firebase");
 }
 
-function saveToFirebase(email) {
-    var emailObject = {
-        email: email
+function saveToFirebase(data) {
+    var dataObject = {
+        DATA: data
     };
 
-    firebase.database().ref('subscription-entries').push().set(emailObject)
+    firebase.database().ref('Data_Entries').push().set(dataObject)
         .then(function(snapshot) {
             success(); // some success method
         }, function(error) {
