fe37fec5ad5d33c486075694bcf340f34c54c7a8
Peake <gepeake@gmail.com>
2018-10-11 17:45:31 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ function logURL(requestDetails) {
   console.log("Loading: " + requestDetails.url);
 }
 
+console.log("Test");
+
 browser.webRequest.onBeforeRequest.addListener(
   logURL,
   {urls: ["<all_urls>"]}
