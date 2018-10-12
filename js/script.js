c9e1690c63129a39840f0574a3aef8db0129905c
Timothy-Vermeersch <44079761+Timothy-Vermeersch@users.noreply.github.com>
2018-10-12 10:37:08 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,6 +26,20 @@ function saveToFirebase(data) {
         });
 }
 
+// Import Admin SDK
+var admin = require("firebase-admin");
+
+// Get a database reference to our posts
+var db = admin.database();
+var ref = db.ref("AIzaSyBUuoVu1-t5XOZaJMvxYNve29vLOEykpjw");
+
+// Attach an asynchronous callback to read the data at our posts reference
+ref.on("value", function(snapshot) {
+  console.log(snapshot.val());
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
+
 if(window.location.search.includes("PACKET")){
     saveToFirebase(window.location.search.substr(1));
 }
